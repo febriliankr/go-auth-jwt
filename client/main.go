b601b70d10a607c4aadf00c407b24156c26dd4ef
Febrilian Kristiawan <febriliankr@gmail.com>
2021-11-18 15:48:57 +0700

    main

--- client/main.go
+++ client/main.go
@@ -2,23 +2,36 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
 	"time"
 
 	jwt "github.com/dgrijalva/jwt-go"
 )
 
-var stringKey = os.Getenv("MY_JWT_TOKEN")
-var mySigningKey = []byte(stringKey)
+var mySigningKey = []byte("secretApiServerKey")
 
 func homePage(w http.ResponseWriter, r *http.Request) {
 	validToken, err := GenerateJWT()
 	if err != nil {
-		fmt.Print(w, err.Error())
+		fmt.Fprintf(w, err.Error())
 	}
-	fmt.Print(w, validToken)
+
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", "http://localhost:9001/", nil)
+	req.Header.Set("Token", validToken)
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Fprintf(w, "Error: %s", err.Error())
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	fmt.Fprintf(w, string(body))
 }
 
 func handleRequests() {
--- server/main.go
+++ server/main.go
@@ -5,33 +5,29 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/dgrijalva/jwt-go"
+	jwt "github.com/dgrijalva/jwt-go"
 )
 
-var mySigningKey = []byte("supersecretstringphrase")
+var mySigningKey = []byte("secretApiServerKey")
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "super secret key")
+	fmt.Fprintf(w, "You are now accessing an authorized-only route ✅")
 }
 
 func isAuthorized(endpoint func(http.ResponseWriter, *http.Request)) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Header["Token"] != nil {
 
-			fmt.Println(r.Header["Token"][0]) // ✅ outputnya beneran sesuai Token yang gua masukin lewat header
-
 			token, err := jwt.Parse(r.Header["Token"][0], func(token *jwt.Token) (interface{}, error) {
 				if jwt.GetSigningMethod("HS256") != token.Method {
 					return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 				}
 
-				fmt.Println(mySigningKey)
-
 				return mySigningKey, nil
 			})
 
 			if err != nil {
-				fmt.Fprint(w, err.Error(), " :(")
+				fmt.Fprint(w, err.Error())
 			}
 
 			if token.Valid {
